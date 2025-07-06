00d24b07e92f041d826ee1a5624593533b8f4614
moran <danielpotoco@gmail.com>
2025-07-05 21:41:37 -0300

    refactor

--- courses/freecode_camp_expressjs/expressro/routes/people_route_w_controller.js
+++ courses/freecode_camp_expressjs/expressro/routes/people_route_w_controller.js
@@ -2,18 +2,15 @@ const router = require('express').Router();
 const people = require('../../methodsap/people');
 const personController = require('../controllers/person_controller');
 
-router.get('/', personController.getAll)
-
-router.get('/:id', personController.get)
-
-router.post('/', personController.post)
-
-router.post('/postman', personController.postToPostman)
-
-router.put('/', personController.put)
-
-router.delete('/:id', personController.deleteById)
-
-router.delete('/', personController.delete)
+router.route('/')
+    .get(personController.getAll)
+    .post(personController.post)
+    .put(personController.put)
+    .delete(personController.delete)
+router.route('/:id')
+    .get(personController.get)
+    .delete(personController.deleteById)
+router.route('/postman')
+    .post(personController.postToPostman)
 
 module.exports = router
